1f40d07e7d73d9671d31c5e6b385c4dc3a714674
Mark Rushakoff <mark@influxdata.com>
2018-09-02 17:38:11 -0700

    Simplify test case

--- README.md
+++ README.md
@@ -18,8 +18,6 @@ go version devel +be10ad7622 Wed Aug 22 22:26:40 2018 +0000 darwin/amd64
 I observed the following with benchstat:
 
 ```
-name                old time/op  new time/op  delta
-BboltWrites/10-4     298µs ±15%  8134µs ± 6%  +2626.85%  (p=0.000 n=10+9)
-BboltWrites/100-4    280µs ± 1%  8001µs ± 2%  +2758.37%  (p=0.000 n=8+8)
-BboltWrites/1000-4   311µs ± 7%  8304µs ± 5%  +2570.40%  (p=0.000 n=10+9)
+name           old time/op  new time/op  delta
+BboltWrites-4   281µs ± 1%  8283µs ±11%  +2843.16%  (p=0.000 n=8+10)
 ```
--- issue_test.go
+++ issue_test.go
@@ -2,57 +2,33 @@ package goissue27415_test
 
 import (
 	"encoding/binary"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"sync"
 	"testing"
 
 	bolt "github.com/coreos/bbolt"
 )
 
 func BenchmarkBboltWrites(b *testing.B) {
-	parallelCount := []int{10, 100, 1000}
-	for _, c := range parallelCount {
-		b.Run(fmt.Sprint(c), func(b *testing.B) {
-			dir, err := ioutil.TempDir("", "")
-			if err != nil {
-				b.Fatal(err)
-			}
-
-			path := filepath.Join(dir, "bolt.db")
-			defer os.Remove(path)
-
-			db, err := bolt.Open(path, 0644, nil)
-			if err != nil {
-				b.Fatal(err)
-			}
-			defer db.Close()
-
-			ch := make(chan uint64, c)
-			var wg sync.WaitGroup
-			wg.Add(c)
-			for i := 0; i < c; i++ {
-				go write(b, db, ch, &wg)
-			}
-
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				ch <- uint64(i)
-			}
-			close(ch)
-			wg.Wait()
-		})
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		b.Fatal(err)
 	}
-}
 
-func write(b *testing.B, db *bolt.DB, ch <-chan uint64, wg *sync.WaitGroup) {
-	defer wg.Done()
+	path := filepath.Join(dir, "bolt.db")
+	defer os.Remove(path)
 
-	for n := range ch {
-		var buf [8]byte
-		binary.BigEndian.PutUint64(buf[:], n)
+	db, err := bolt.Open(path, 0644, nil)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer db.Close()
+
+	var buf [8]byte
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		binary.BigEndian.PutUint64(buf[:], uint64(i))
 
 		if err := db.Update(func(tx *bolt.Tx) error {
 			// Create a bucket.
