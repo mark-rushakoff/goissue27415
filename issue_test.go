ffbf3e8c4787226cb8ce00b7228c5a62c88939db
Mark Rushakoff <mark@influxdata.com>
2018-09-02 21:40:04 -0700

    Add test that operates similar to benchmark
    The test just does 3 write operations, so it's perhaps more useful when
    you want to just view a trace, or use strace/dtruss, etc.

--- issue_test.go
+++ issue_test.go
@@ -15,6 +15,7 @@ func BenchmarkBboltWrites(b *testing.B) {
 	if err != nil {
 		b.Fatal(err)
 	}
+	defer os.RemoveAll(dir)
 
 	path := filepath.Join(dir, "bolt.db")
 	defer os.Remove(path)
@@ -31,18 +32,47 @@ func BenchmarkBboltWrites(b *testing.B) {
 		binary.BigEndian.PutUint64(buf[:], uint64(i))
 
 		if err := db.Update(func(tx *bolt.Tx) error {
-			// Create a bucket.
 			bucket, err := tx.CreateBucketIfNotExists([]byte("numbers"))
 			if err != nil {
 				return err
 			}
 
-			if err := bucket.Put(buf[:], []byte("ok")); err != nil {
-				return err
-			}
-			return nil
+			return bucket.Put(buf[:], []byte("ok"))
 		}); err != nil {
 			b.Error(err)
+		}
+	}
+}
+
+func TestBboltWrites(t *testing.T) {
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	path := filepath.Join(dir, "bolt.db")
+	defer os.Remove(path)
+
+	db, err := bolt.Open(path, 0644, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	var buf [8]byte
+	for i := 0; i < 3; i++ {
+		binary.BigEndian.PutUint64(buf[:], uint64(i))
+
+		if err := db.Update(func(tx *bolt.Tx) error {
+			bucket, err := tx.CreateBucketIfNotExists([]byte("numbers"))
+			if err != nil {
+				return err
+			}
+
+			return bucket.Put(buf[:], []byte("ok"))
+		}); err != nil {
+			t.Fatal(err)
 		}
 	}
 }
